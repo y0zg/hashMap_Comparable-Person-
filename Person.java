8150c43eefdf4c0b821145c31c1ea0929388097d
y0zh <webstd.design@gmail.com>
2016-05-29 19:30:50 +0300

    Singleton

--- Person.java
+++ Person.java
@@ -10,6 +10,36 @@ public class Person implements Comparable<Person>
 	private String lastName;
 	private UUID id;
 
+	//create an object of Person
+	private static Person personInstance = new Person();
+
+	//make the constructor private so that this class cannot be
+	//instantiated
+	private Person() {
+	}
+
+	//Get the only object available
+	public static Person getPersonInstance() {
+		return personInstance;
+	}
+
+	private void fillAndSearch() {
+		Person person = null;
+		Map<Person, String> mymap = new HashMap<>();
+		for (int i = 0; i < LIMIT; i++) {
+			UUID randomUUID = UUID.randomUUID();
+			person = new Person("fn", "ln", randomUUID);
+			mymap.put(person, "comment" + i);
+			System.out.println(mymap.get(person));
+		}
+		long start = System.currentTimeMillis();
+		mymap.get(person);
+		long stop = System.currentTimeMillis();
+
+		System.out.println(stop - start + " millis #1");
+	}
+
+	//end new <code>
 
 	public Person(String firsName, String lastName, UUID id) {
 		this.firsName = firsName;
@@ -32,19 +62,10 @@ public class Person implements Comparable<Person>
 
 
 	public static void main(String[] args) {
-		Person person = null;
 		long start = System.currentTimeMillis();
-		Map<Person, String> mymap = new HashMap<>();
-		for (int i = 0; i < LIMIT; i++) {
-			UUID randUUID = UUID.randomUUID();
-			person = new Person("fn", "ln", randUUID);
-			mymap.put(person, "comment" + i);
-			System.out.println(mymap.get(person));
-		}
-
-		mymap.get(person);
+		Person persobject = Person.getPersonInstance();
+		persobject.fillAndSearch();
 		long stop = System.currentTimeMillis();
-
-		System.out.println(stop - start + "msec");
+		System.out.println(stop - start + " millis #2");
 	}
 }
